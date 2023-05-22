dd765cbda33138ed0ecc523764a1a92eec8b31c0
pausc <pau.cusco@estudiantat.upc.edu>
2023-05-22 11:39:39 +0200

    actualització treball

--- treball_comp/decode.py
+++ treball_comp/decode.py
@@ -1,70 +1,75 @@
 import math
-import sys
 import os
+import sys
+from bitarray import bitarray
 
 def dec2binary(a, k):
     binary = bin(a)[2:]
     padded_binary = '0'*(k-len(binary)) + binary  
     return padded_binary
 
-def decoder(encoded, n):
-    txt_len = int(encoded[:32],2)
-    alpha_len = int(encoded[32:48], 2) * 7
-    print("El txt_len és ", txt_len, " i el alpha_len és ", alpha_len)
+def decode(code, n):
+    len_txt = int(code[0:32], 2)
+    len_alpha = int(code[32:48], 2)
+    alpha_bin = code[48:48+len_alpha*7]
+    C = code[48+len_alpha*7:]
     alpha = []
-    src = {}
-    total = 0
-    for i in range(48, 48+alpha_len, 7):
-        lletra = int(encoded[i:i+7], 2)
-        lletra = chr(lletra)
+    for i in range (0, len_alpha*7, 7):
+        bin = alpha_bin[i:i+7]
+        lletra = chr(int(bin, 2))
         alpha.append(lletra)
-        src.update({lletra:1})
-        total += 1
-    code = encoded[48+alpha_len:]
-    print("El codi és:", code)
-    print("El source és:", src, " i l'alpha és:", alpha, " i el total és ", total)
+    
+    it_alpha = 1
+    src = {'\x1b':1}
+    total = 1
 
-    k = 8
     txt = ''
+    lletra = ''
+    k = 32
     a = 0
     b = 2**k-1
     count = k-1
     end = False
-    y = int(code[0:k], 2)
+    y = int(C[0:k], 2)
 
-    print("Alpha, gamma i beta inicials:", a, y, b)
-    while 9 != len(txt):
+    #print("Alpha, gamma i beta inicials:", a, y, b)
+
+    while len(txt) != len_txt:
         p = 0
         aux_p = 0
         d = b-a+1
         for clave in src.keys():
             aux_p = p
-            p += src[clave]/total
-            if a+math.floor(d * p) > y:
-                print(clave)
-                src[clave] += 1
-                txt += clave
-                total += 1
-                print(src, total)
+            p += src[clave]
+            if a+math.floor(d * p/total) > y:
+                lletra = clave
                 break
-        print("Determinen nova lletra:", txt[len(txt)-1])
-        b = a + math.floor(d * p)-1
-        a = a + math.floor(d * aux_p) 
-        print("Nous alpha, gamma i beta:", a, y, b)
+        #print("Determinen nova lletra:", lletra)
+        if lletra == '\x1b':
+            lletra = alpha[it_alpha]
+            it_alpha += 1
+            #print("Determinen la verdadera nova lletra:", lletra)
+            src.update({lletra:0})
+        txt += lletra
+        src[lletra] = src[lletra]+1
+        b = a + math.floor(d * p/total)-1
+        a = a + math.floor(d * aux_p/total) 
+        total += 1
+        #print("Nous alpha, gamma i beta:", a, y, b)
         a_bin = dec2binary(a, k)
         y_bin = dec2binary(y, k)
         b_bin = dec2binary(b, k)
-        print("En binari:", a_bin, y_bin, b_bin)
+        #print("En binari:", a_bin, y_bin, b_bin)
 
         while (a_bin[0] == b_bin[0]):
-            if count < len(code)-1:
+            if count < len(C)-1:
                 count += 1
             else:
                 end = True
             char = a_bin[0]
             a = a*2
             b = b*2 +1
-            if code[count] == '0' or end:
+            if C[count] == '0' or end:
                 y = y*2
             else:
                 y = y*2+1
@@ -75,35 +80,38 @@ def decoder(encoded, n):
             a_bin = dec2binary(a, k)
             y_bin = dec2binary(y, k)
             b_bin = dec2binary(b, k)
-            print ("Reescalat:", a_bin, y_bin, b_bin)
+            #print ("Reescalat:", a_bin, y_bin, b_bin)
             
         if a_bin[1] == '1' and b_bin[1] == '0':
             while(not(a_bin[0:2] == '00' or b_bin[0:2] == '11')):
-                if count < len(code)-1:
+                if count < len(C)-1:
                     count += 1
                 else:
                     end = True
                 a = a*2-2**(k-1)
                 b = b*2-2**(k-1)+1
-                if code[count] == '0' or end:
+                if C[count] == '0' or end:
                     y = y*2-2**(k-1)
                 else:
                     y = y*2-2**(k-1)+1
                 a_bin = dec2binary(a, k)
                 y_bin = dec2binary(y, k)
                 b_bin = dec2binary(b, k)
-                print ("Underflow:", a_bin, y_bin, b_bin)
+                #print ("Underflow:", a_bin, y_bin, b_bin)
         
-        print("Queden alpha, gamma i beta:", a, y, b)
-        print()
-    print(txt)
+        #print("Queden alpha, gamma i beta:", a, y, b)
+        #print()
     return txt
 
+
 fileName = sys.argv[1]
-with open(fileName, 'r') as f:
-    txt = f.read()
 
-decoded = decoder(txt, 1)
+with open(fileName + ".cdi", 'rb') as f:
+    bits = bitarray()
+    bits.fromfile(f)
 
-with open(os.path.splitext(fileName)[0] + ".og", 'w') as f:
+txt = bits.to01()
+decoded = decode(txt, 1)
+
+with open(os.path.splitext(fileName)[0] + "-desc.txt", 'w', encoding='utf-8') as f:
     f.write(decoded)
--- treball_comp/encode.py
+++ treball_comp/encode.py
@@ -1,4 +1,7 @@
 import math
+import sys
+import os
+from bitarray import bitarray
 
 def dec2binary(a, k):
     binary = bin(a)[2:]
@@ -12,26 +15,26 @@ def list2code(a):
         c += dec2binary(dec, 7)
     return c
 
-def encoder (txt, n):
-    total = 0
-    k = 8
+def encode(txt,n):
+    src = {'\x1b':1}
+    total = 1
+    k = 32
     a = 0
     b = 2**k-1
-    print("Interval inicial", a, b)
-    print()
+    #print("Interval inicial", a, b)
+    #print()
     c = ''
-    alpha = sorted(list(set(txt)))
+    alpha = ['\x1b']
     count = 0
-    src = {}
-    
-    for i in alpha:
-        src.update({i:1})
-        total+=1
+    aux = ''
+
     for i in range(0, len(txt), n):
-        
         lletra = txt[i:i+n]
-        
-        
+        aux = lletra
+
+        if lletra not in src:
+            #print("La lletra " + lletra + " no estava en el diccionari, s'ha afegit")
+            lletra = '\x1b'     
         d = b-a+1
         it = alpha.index(lletra)
         p = 0
@@ -40,11 +43,10 @@ def encoder (txt, n):
             it -= 1
         b = a + math.floor(d * (p + src[lletra])/total)-1
         a = a + math.floor(d * p/total)    
-
-        print("Es codifica la lletra:", lletra, " i queda el subinterval", a, b)
+        #print("Es codifica la lletra:", lletra, " i queda el subinterval", a, b)
         a_bin = dec2binary(a, k)
         b_bin = dec2binary(b, k)
-        print("En binari:", a_bin, b_bin)
+#        print("En binari:", a_bin, b_bin)
 
         while (a_bin[0] == b_bin[0]):
             char = a_bin[0]
@@ -56,10 +58,10 @@ def encoder (txt, n):
                 b = b - 2**k
             a_bin = dec2binary(a, k)
             b_bin = dec2binary(b, k)
-            print ("Reescalat:", a_bin, b_bin)
-            print("codi:", c)
+            #print ("Reescalat:", a_bin, b_bin)
+            #print("codi:", c)
             if count > 0:
-                print("Afegits", count, "bits d'underflow")
+              #  print("Afegits", count, "bits d'underflow")
                 if char == '0':
                     char = '1'
                 else:
@@ -67,7 +69,7 @@ def encoder (txt, n):
                 while(count > 0):
                     c += char
                     count -= 1
-                print("codi:", c)
+               # print("codi:", c)
 
         if a_bin[1] == '1' and b_bin[1] == '0':
             while(not(a_bin[0:2] == '00' or b_bin[0:2] == '11')):
@@ -76,36 +78,42 @@ def encoder (txt, n):
                 b = b*2-2**(k-1)+1
                 a_bin = dec2binary(a, k)
                 b_bin = dec2binary(b, k)
-                print("Underflow num.", count)
-                print ("Reescalat:", a_bin, b_bin)
-
-        print("Interval ha quedat", a, b)
-        print()
+        #        print("Underflow num.", count)
+        #        print ("Reescalat:", a_bin, b_bin)
+        #print("Interval ha quedat", a, b)
+        #print()
+        if aux not in src:
+            src.update({aux:1})
+            alpha.append(aux)          
+        else:
+            src[aux]+=1
         total+=n
-        src.update({lletra:src[lletra]+1})
-    
+
     c += '1'
-    print("afegim un 1 al final i queda el codi", c)
+    #print("afegim un 1 al final i queda el codi", c)
     code = dec2binary(len(txt), 32)
-    print("El text te una mida de ", len(txt) ,  " i el code és " + code)
+    #print("El text te una mida de ", len(txt) ,  " i el code és " + code)
     code += dec2binary(len(alpha), 16)
-    print("Alpha te una mida de ", len(alpha), " i és ", alpha , " i codificat és ", list2code(alpha))
+    #print("Alpha te una mida de ", len(alpha), " i és ", alpha , " i codificat és ", list2code(alpha))
     code += list2code(alpha)
     code += c
-    print("El codi final és " + code)
-    print("EL source és ", src)
+    #print("El codi final és " + code)
     return code
+    
 
 
-import sys
-import os
 
 fileName = sys.argv[1]
-with open(fileName, 'r') as f:
+
+with open(fileName + ".txt", 'r', encoding='utf-8') as f:
     txt = f.read()
 
-encoded = encoder(txt, 1)
+encoded = encode(txt, 1)
+bits = bitarray(encoded)
 
-with open(os.path.splitext(fileName)[0] + ".codi", 'w') as f:
-    f.write(encoded)
+with open(os.path.splitext(fileName)[0] + ".cdi", 'wb') as f:
+    bits.tofile(f)
 
+print ("El fitxer s'ha comprimit a " + fileName + ".cdi.")
+rendiment = 8 * os.path.getsize(fileName+".cdi") / len(txt)
+print ("El rendiment en la compressió és ", rendiment )
